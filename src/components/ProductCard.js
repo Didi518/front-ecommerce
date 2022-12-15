0ce4fd44fcaa2df09c9e15ebc25e2957eb0171a0
Didi518 <94155950+Didi518@users.noreply.github.com>
2022-12-15 08:50:41 +0100

    store en dur ok

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,47 +1,122 @@
 import React from 'react';
 import ReactStars from 'react-rating-stars-component';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
-const ProductCard = () => {
+const ProductCard = (props) => {
+  const { grid } = props;
+  const location = useLocation();
+
   return (
-    <div className="col-3">
-      <Link className="product-card position-relative">
-        <div className="wishlist-icon position-absolute">
-          <Link>
-            <img src="images/wish.svg" alt="wishlist" />
-          </Link>
-        </div>
-        <div className="product-image">
-          <img className="img-fluid" src="images/watch.jpg" alt="article" />
-          <img className="img-fluid" src="images/watch-1.avif" alt="article" />
-        </div>
-        <div className="product-details">
-          <h6 className="brand">Havels</h6>
-          <h5 className="product-title">Casque enfant, 10 couleurs au choix</h5>
-          <ReactStars
-            count={5}
-            value={3}
-            edit={false}
-            size={24}
-            activeColor="#ffd700"
-          />
-          <p className="price">100.00€</p>
-        </div>
-        <div className="action-bar position-absolute">
-          <div className="d-flex flex-column gap-15">
+    <>
+      <div
+        className={`${
+          location.pathname === '/boutique' ? `gr-${grid}` : 'col-3'
+        }`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
             <Link>
-              <img src="images/prodcompare.svg" alt="comparer" />
-            </Link>
-            <Link>
-              <img src="images/view.svg" alt="view" />
-            </Link>
-            <Link>
-              <img src="images/add-cart.svg" alt="ajouter au panier" />
+              <img src="images/wish.svg" alt="wishlist" />
             </Link>
           </div>
-        </div>
-      </Link>
-    </div>
+          <div className="product-image">
+            <img className="img-fluid" src="images/watch.jpg" alt="article" />
+            <img
+              className="img-fluid"
+              src="images/watch-1.avif"
+              alt="article"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Casque enfant, 10 couleurs au choix
+            </h5>
+            <ReactStars
+              count={5}
+              value={3}
+              edit={false}
+              size={24}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+              fugiat iusto fuga praesentium.
+            </p>
+            <p className="price">100.00€</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="comparer" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="ajouter au panier" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div
+        className={`${
+          location.pathname === '/boutique' ? `gr-${grid}` : 'col-3'
+        }`}
+      >
+        <Link className="product-card position-relative">
+          <div className="wishlist-icon position-absolute">
+            <Link>
+              <img src="images/wish.svg" alt="wishlist" />
+            </Link>
+          </div>
+          <div className="product-image">
+            <img className="img-fluid" src="images/watch.jpg" alt="article" />
+            <img
+              className="img-fluid"
+              src="images/watch-1.avif"
+              alt="article"
+            />
+          </div>
+          <div className="product-details">
+            <h6 className="brand">Havels</h6>
+            <h5 className="product-title">
+              Casque enfant, 10 couleurs au choix
+            </h5>
+            <ReactStars
+              count={5}
+              value={3}
+              edit={false}
+              size={24}
+              activeColor="#ffd700"
+            />
+            <p className={`description ${grid === 12 ? 'd-block' : 'd-none'}`}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+              mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+              voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+              fugiat iusto fuga praesentium.
+            </p>
+            <p className="price">100.00€</p>
+          </div>
+          <div className="action-bar position-absolute">
+            <div className="d-flex flex-column gap-15">
+              <Link>
+                <img src="images/prodcompare.svg" alt="comparer" />
+              </Link>
+              <Link>
+                <img src="images/view.svg" alt="view" />
+              </Link>
+              <Link>
+                <img src="images/add-cart.svg" alt="ajouter au panier" />
+              </Link>
+            </div>
+          </div>
+        </Link>
+      </div>
+    </>
   );
 };
 
--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,13 +1,267 @@
-import React from 'react';
+import React, { useState } from 'react';
+import ReactStars from 'react-rating-stars-component';
 import BreadCrumb from '../components/BreadCrumb';
 import Meta from '../components/Meta';
+import ProductCard from '../components/ProductCard';
 
 const OurStore = () => {
+  const [grid, setGrid] = useState(4);
+
   return (
     <>
       <Meta title={'Boutique'} />
       <BreadCrumb title="Boutique" />
-      <div className="store-wrapper"></div>
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Choix Par Catégories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Montre</li>
+                    <li>Tv</li>
+                    <li>Photo</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Tri Par</h3>
+                <div>
+                  <h5 className="sub-title">Disponibilité</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                        name=""
+                        value=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        En Stock (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        type="checkbox"
+                        className="form-check-input"
+                        name=""
+                        value=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        Indisponible (0)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+                <h5 className="sub-title">Prix</h5>
+                <div className="d-flex align-items-center gap-10">
+                  <div className="form-floating">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="floatingInput"
+                      placeholder="De"
+                    />
+                    <label htmlFor="floatingInput">De</label>
+                  </div>
+                  <div className="form-floating">
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="floatingInput1"
+                      placeholder="A"
+                    />
+                    <label htmlFor="floatingInput1">A</label>
+                  </div>
+                </div>
+                <h5 className="sub-title">Couleurs</h5>
+                <div>
+                  <ul className="colors ps-0">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+                <h5 className="sub-title">Tailles</h5>
+                <div>
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name=""
+                      value=""
+                      id="color-1"
+                    />
+                    <label className="form-check-label" htmlFor="color-1">
+                      S (2)
+                    </label>
+                  </div>
+                  <div className="form-check">
+                    <input
+                      type="checkbox"
+                      className="form-check-input"
+                      name=""
+                      value=""
+                      id="color-2"
+                    />
+                    <label className="form-check-label" htmlFor="color-2">
+                      M (2)
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Tags des Articles</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Casque
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Cable
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Séléction Aléatoire</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Casque enfant, 10 couleurs au choix</h5>
+                      <ReactStars
+                        count={5}
+                        value={3}
+                        edit={false}
+                        size={24}
+                        activeColor="#ffd700"
+                      />
+                      <b>300€</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Casque enfant, 10 couleurs au choix</h5>
+                      <ReactStars
+                        count={5}
+                        value={3}
+                        edit={false}
+                        size={24}
+                        activeColor="#ffd700"
+                      />
+                      <b>300€</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-9">
+              <div className="filter-sort-grid mb-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="mb-0 d-block" style={{ width: '100px' }}>
+                      Trier Par:
+                    </p>
+                    <select name="" className="form-control form-select" id="">
+                      <option value="manual">Incontournable</option>
+                      <option value="best-selling" selected="selected">
+                        Top Vente
+                      </option>
+                      <option value="title-ascending">Alphabétique, A-Z</option>
+                      <option value="title-descending">
+                        Alphabétique, Z-A
+                      </option>
+                      <option value="price-ascending">Prix, croissant</option>
+                      <option value="price-descending">
+                        Prix, décroissant
+                      </option>
+                      <option value="created-ascending">
+                        Parution, récent vers ancienne
+                      </option>
+                      <option value="created-descending">
+                        Parution, ancienne vers récente
+                      </option>
+                    </select>
+                  </div>
+                  <div className="d-flex align-items-center gap-10">
+                    <p className="totalproducts mb-0">21 Articles</p>
+                    <div className="d-flex gap-10 align-items-center grid">
+                      <img
+                        onClick={() => {
+                          setGrid(3);
+                        }}
+                        src="images/gr4.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(4);
+                        }}
+                        src="images/gr3.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(6);
+                        }}
+                        src="images/gr2.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                      <img
+                        onClick={() => {
+                          setGrid(12);
+                        }}
+                        src="images/gr.svg"
+                        className="d-block img-fluid"
+                        alt="grid"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="products-list pb-5">
+                <div className="d-flex gap-10 flex-wrap">
+                  <ProductCard grid={grid} />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
